d5a026597f68c412a92a9fb930668918fabb1bc4
Caio Avelar <caioavelar17@hotmail.com>
2020-10-16 14:54:08 -0300

    Publicação

---
+++ docs/build/precache-manifest.c7d08123c50951c45217a54b819cb5aa.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70b280f44bef2153870829818db41978",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84d05515be548fc4dd14",
+    "url": "/static/css/2.316749da.chunk.css"
+  },
+  {
+    "revision": "fa44f2d8283d0580de2a",
+    "url": "/static/css/main.daea5d8b.chunk.css"
+  },
+  {
+    "revision": "84d05515be548fc4dd14",
+    "url": "/static/js/2.924ea8c0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.924ea8c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa44f2d8283d0580de2a",
+    "url": "/static/js/main.52f3d6e4.chunk.js"
+  },
+  {
+    "revision": "30230bc049e364f4fdd8",
+    "url": "/static/js/runtime-main.67eec890.js"
+  },
+  {
+    "revision": "efad0a097ae5a8a8c519244b44f1b584",
+    "url": "/static/media/danielTurismo.efad0a09.jpg"
+  },
+  {
+    "revision": "23a27d192c12a0160d15f877a69872ae",
+    "url": "/static/media/jobsmart.23a27d19.png"
+  },
+  {
+    "revision": "cda8a988ffeaf845b1684eb1f00fe045",
+    "url": "/static/media/minions.cda8a988.png"
+  },
+  {
+    "revision": "c762d850e2e8d0e29047608715196736",
+    "url": "/static/media/roboto-all-400-italic.c762d850.woff"
+  },
+  {
+    "revision": "a91ad097d24828af724d4fee36a063ed",
+    "url": "/static/media/roboto-all-400-normal.a91ad097.woff"
+  },
+  {
+    "revision": "8266321ab43353bcd147ad67600d165c",
+    "url": "/static/media/roboto-cyrillic-400-italic.8266321a.woff2"
+  },
+  {
+    "revision": "8bb64952764a884d67019b3486296ab9",
+    "url": "/static/media/roboto-cyrillic-400-normal.8bb64952.woff2"
+  },
+  {
+    "revision": "b3e580d221a4722c959155878ab94210",
+    "url": "/static/media/roboto-cyrillic-ext-400-italic.b3e580d2.woff2"
+  },
+  {
+    "revision": "4743c758a952f2bd4a35d4e42afc002b",
+    "url": "/static/media/roboto-cyrillic-ext-400-normal.4743c758.woff2"
+  },
+  {
+    "revision": "469a78405c3ae073ba769321fa0584f3",
+    "url": "/static/media/roboto-greek-400-italic.469a7840.woff2"
+  },
+  {
+    "revision": "c1e9793c84cb26c44ef2a2cf8b6f49ce",
+    "url": "/static/media/roboto-greek-400-normal.c1e9793c.woff2"
+  },
+  {
+    "revision": "801b64f75ae26dab9c36f00629ebdad0",
+    "url": "/static/media/roboto-greek-ext-400-italic.801b64f7.woff2"
+  },
+  {
+    "revision": "182ee6a4872ca8fa78048951b1561a5c",
+    "url": "/static/media/roboto-greek-ext-400-normal.182ee6a4.woff2"
+  },
+  {
+    "revision": "51521a2a8da71e50d871ac6fd2187e87",
+    "url": "/static/media/roboto-latin-400-italic.51521a2a.woff2"
+  },
+  {
+    "revision": "479970ffb74f2117317f9d24d9e317fe",
+    "url": "/static/media/roboto-latin-400-normal.479970ff.woff2"
+  },
+  {
+    "revision": "dc3871f486e189164db4b98968330bc4",
+    "url": "/static/media/roboto-latin-ext-400-italic.dc3871f4.woff2"
+  },
+  {
+    "revision": "455200cb007fe1212c668721d827c691",
+    "url": "/static/media/roboto-latin-ext-400-normal.455200cb.woff2"
+  },
+  {
+    "revision": "9780bde87bceec579eaf16bddad47615",
+    "url": "/static/media/roboto-vietnamese-400-italic.9780bde8.woff2"
+  },
+  {
+    "revision": "a8be5b46d06bb541b0968196ee5e6bb8",
+    "url": "/static/media/roboto-vietnamese-400-normal.a8be5b46.woff2"
+  }
+]);
