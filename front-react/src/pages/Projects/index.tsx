f23a882b2da0be4fb1f2a015507bb5e34eab354a
Caio Avelar <caioavelar17@hotmail.com>
2020-10-16 14:44:06 -0300

    Adicionado projeto Daniel Turismo

--- front-react/src/pages/Projects/index.tsx
+++ front-react/src/pages/Projects/index.tsx
@@ -2,24 +2,33 @@ import React, { useEffect } from 'react';
 import ProjectCard from './ProjectCard';
 import jobsmart from '../../assets/projects/jobsmart.png';
 import minions from '../../assets/projects/minions.png';
+import danielTurismo from '../../assets/projects/danielTurismo.jpg';
 import './styles.css';
+import { Grid } from '@material-ui/core';
 
 const Projects = () => {
     useEffect(() => {
         console.log('Fui renderizado')
     }, [])
     return (
-        <div className="main-projects">
-            <ProjectCard 
-                projectName="JOBSMART"
-                innerText="Projeto desenvolvido para a UTRAMIG"
-                image={jobsmart}/>
-            <ProjectCard 
-                projectName="24HoursOfHappy"
-                innerText="Réplica do 24HoursOfHappy"
-                image={minions}
-                projectURL="http://24hoursofhappy.batcave.net/"/>
-        </div>
+        <>
+            <Grid container direction="row" alignContent="center" justify="center">
+                    <ProjectCard 
+                        projectName="JOBSMART"
+                        innerText="Projeto desenvolvido para a UTRAMIG"
+                        image={jobsmart}/>
+                    <ProjectCard 
+                        projectName="24HoursOfHappy"
+                        innerText="Réplica do site 24HoursOfHappy, que reproduzia um clipe da música Happy a cada momento do dia, 24 horas"
+                        image={minions}
+                        projectURL="http://24hoursofhappy.batcave.net/"/>
+                    <ProjectCard 
+                        projectName="Daniel Turismo"
+                        innerText="Página estática para a divulgação de uma empresa de viagens e fretamentos de Belo Horizonte"
+                        image={danielTurismo}
+                        projectURL="http://danielturismo.com/"/>
+            </Grid>
+        </>
     );
 }
 
